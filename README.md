287dfc2200996474a3fe6fb14fd5c6c21ef380ae
eagle <eagle.lam.le@Gmail.com>
2021-08-25 10:30:45 -0400

    logo save

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mood-Application-v2
---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { LoginPage } from './components/LoginPage';
+import { SignupPage } from './components/SignupPage';
+import {HomePage} from './components/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  return (
+
+   <>
+      <Switch>
+                <Route path="/" component={HomePage} exact />
+
+                <Route path="/login" > <LoginPage/> </Route>
+                <Route path="/signup" component={SignupPage} />
+            </Switch>
+
+    
+   </>
+  );
+}
+
+export default App;

fef8c1c6bd24d5a9eea524a3181c6af106b359b1
Ebad Rehman <Ebadrehman92@gmail.com>
2021-08-29 13:12:59 -0400

    just for the pull request

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,28 @@
-import { LoginPage } from './components/LoginPage';
-import { SignupPage } from './components/SignupPage';
-import {HomePage} from './components/HomePage';
+import { LoginPage } from "./components/LoginPage";
+import { SignupPage } from "./components/SignupPage";
+import { HomePage } from "./components/HomePage";
 // import {Navbar} from './components/Navbar';
 
 // import {Cover} from './components/Cover';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { AllMoods } from './components/AllMoods';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { AllMoods } from "./components/AllMoods";
 
-function App() {  
-
+function App() {
   return (
-
-   
-    
-  
-   
-      <Switch>
-                <Route path="/" component={()=>(<HomePage />)} exact />
-                <Route path="/allMoods" > <AllMoods/> </Route>
-                <Route path="/login" > <LoginPage/> </Route>
-                <Route path="/signup" component={SignupPage} />
-
-               
-                
-            </Switch>
-
-   
-
-    
-   
+    <Switch>
+      <Route path="/" component={() => <HomePage />} exact />
+      <Route path="/allMoods">
+        {" "}
+        <AllMoods />{" "}
+      </Route>
+      <Route path="/login">
+        {" "}
+        <LoginPage />{" "}
+      </Route>
+      <Route path="/signup" component={SignupPage} />
+    </Switch>
   );
-
- 
 }
-
-
-
-
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import './../css/Navbar.css';
+import React from "react";
+import "./../css/Navbar.css";
 
-export const Navbar = ({currentPage, handlePageChange}) => {
-    return (
-        <nav>
-            <img className ="logo" src = "https://api.freelogodesign.org/files/f077bb1a586a4cc7afd80d94fe0a7eb1/thumb/logo_200x200.png?v=637652428960000000"/>
-            <div>
-           
-                    <a
-                 href="#home"
-                 onClick={()=> handlePageChange('Home')}
-                 className={currentPage === 'Home' ?" nav-link active" : "nav-link"}
-                 >
-                     Mood Journal
-                 </a>
-                
-                    <a 
-                    href="#saved"
-                    onClick={()=> handlePageChange('Saved')}
-                    >Saved Entries
-                    </a>
-                
-                    <a className="mrg" onClick={logout}>Logout</a>
-                    {/* </li>
+export const Navbar = ({ currentPage, handlePageChange }) => {
+  return (
+    <nav>
+      <img
+        className="logo"
+        src="https://api.freelogodesign.org/files/f077bb1a586a4cc7afd80d94fe0a7eb1/thumb/logo_200x200.png?v=637652428960000000"
+      />
+      <div>
+        <a
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? " nav-link active" : "nav-link"}
+        >
+          Mood Journal
+        </a>
+
+        <a href="#saved" onClick={() => handlePageChange("Saved")}>
+          Saved Entries
+        </a>
+
+        <a className="mrg" onClick={logout}>
+          Logout
+        </a>
+        {/* </li>
            </ul> */}
-           </div>
-        </nav>
-    )
+      </div>
+    </nav>
+  );
+};
+
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "/login";
 }
-
-
-
-function logout(){
-    localStorage.removeItem("token");
-    window.location.href="/login";
-
-
-}
-
-

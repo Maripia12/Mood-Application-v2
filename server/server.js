427c2eea054eb9036120d0dec508b4e2cec72767
Ebad Rehman <Ebadrehman92@gmail.com>
2021-08-30 10:31:17 -0400

    added content to server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 
 const logger = require("morgan");
 //const url = "mongodb://localhost/workout";
-const db=require('./config/config').get(process.env.NODE_ENV);
+const db = require("./config/config").get(process.env.NODE_ENV);
 
-const routes = require('./routes/user');
-const routes1 = require('./routes/quote');
-const User=require('./models/user');
-const {auth} =require('./middleware/auth');
+const routes = require("./routes/user");
+const routes1 = require("./routes/quote");
+const User = require("./models/user");
+const { auth } = require("./middleware/auth");
 
 const app = express();
 
@@ -18,14 +18,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  db.DATABASE,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err) {
-    if (err) console.log(err);
-    console.log("database is connected");
-  }
-);
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/mood-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("[i] MongoDB connected successfully");
+  })
+  .catch((err) => {
+    console.log(`[E] MongoDB connection error: ${err}`);
+  });
 
 //app.use(require("./routes/api.js"));
 app.use(routes);

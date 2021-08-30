235f48dc708ee7b89fb1c76a75806e07d218bd9a
Ebad Rehman <Ebadrehman92@gmail.com>
2021-08-30 12:03:50 -0400

    testing

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,25 @@
-const config = {
-  // production :{
-  //     SECRET: process.env.SECRET,
-  //     DATABASE: process.env.MONGODB_URI
-  // },
-  default: {
-    SECRET: "mysecretkey",
-    DATABASE: "mongodb://localhost/users",
-  },
-};
+// const config = {
+//   // production :{
+//   //     SECRET: process.env.SECRET,
+//   //     DATABASE: process.env.MONGODB_URI
+//   // },
+//   default: {
+//     SECRET: "mysecretkey",
+//     DATABASE: "mongodb://localhost/users",
+//   },
+// };
 
-exports.get = function get(env) {
-  return config[env] || config.default;
-};
+// exports.get = function get(env) {
+//   return config[env] || config.default;
+// };
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mood-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const logger = require("morgan");
 //const url = "mongodb://localhost/workout";
-const db = require("./config/config").get(process.env.NODE_ENV);
-
+// const db = require("./config/config").get(process.env.NODE_ENV);
+const db = require("./config/config");
 const routes = require("./routes/user");
 const routes1 = require("./routes/quote");
 const User = require("./models/user");
@@ -17,20 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/mood-db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("[i] MongoDB connected successfully");
-  })
-  .catch((err) => {
-    console.log(`[E] MongoDB connection error: ${err}`);
-  });
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(process.env.MONGODB_URI || "mongodb://localhost/mood-db", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log("[i] MongoDB connected successfully");
+//   })
+//   .catch((err) => {
+//     console.log(`[E] MongoDB connection error: ${err}`);
+//   });
 
 //app.use(require("./routes/api.js"));
 app.use(routes);
